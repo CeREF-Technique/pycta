ad6bdd9919ed7b6cc7fd1c5cf6e2c04704f0be77
JVA <julien.vachaudez@gmail.com>
2018-01-03 17:30:22 +0100

    Added split_visits method

--- pycta.py
+++ pycta.py
@@ -14,12 +14,8 @@
 import os
 import sys
 
+import numpy as np
 import pandas as pd
-
-
-FILEPATH = r"C:\1_CERISIC\CERISIC\Projet CTA\example\fichier_demo.csv"
-
-df = pd.read_csv(FILEPATH,header=1,names=["date","hour","ID","CH4","CO2","weigth","scale"])
 
 
 class CTA():
@@ -57,7 +53,30 @@ class CTA():
     def split_visits(self):
         """
         """
+        visits_index = list()
         
+        # Group DataFrame by IDs
+        grp = self.df.groupby('ID')
+        
+        # List all unique IDs
+        IDs = self.df.ID.unique().tolist()
+        
+        for ID in IDs:
+            prev = 0
+            unique_grp = grp.get_group(ID)
+            
+            # Convert DataFrame to Numpy array
+            index_array = unique_grp.index.values
+            
+            # Split array in visits based on index
+            splits = np.append(np.where(np.diff(index_array) != 1)[0],len(index_array)+1)+1
+            
+            for split in splits:
+                visits_index.append(index_array[prev:split])
+                prev = split
+        
+        for visit in visits_index:
+            self.visits.append(Visit(cta.df.loc[visit.astype(np.int32).tolist()]))
     
     def peaks_detect(self):
         """
@@ -78,7 +97,7 @@ class CTA():
 class Visit(CTA):
     """
     """
-    def __init__(self):
+    def __init__(self,df):
         """
         """
         
@@ -96,7 +115,9 @@ class Visit(CTA):
 
 if __name__ == '__main__':
     
-    FILE_PATH = r"csv\fichier_demo"
+    FILE_PATH = r"csv\fichier_demo2.csv"
     
     cta = CTA()
     cta.read_csv_file(FILE_PATH)
+    
+    cta.split_visits()
