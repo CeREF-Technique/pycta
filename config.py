6f48b40e0490c8e2e6a03aa0b9ac57393d024e18
JVA <julien.vachaudez@gmail.com>
2018-01-04 14:00:18 +0100

    Added configuration file

---
+++ config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##########################################################################
+# File name : config.py
+# Usages : Description of the module
+# Start date : Thu Jan 04 13:09:58 2018
+# Last review : Thu Jan 04 13:09:58 2018
+# Version : 0.1
+# Author(s) : Julien Vachaudez - julien.vachaudez@cerisic.be
+# License : The pycta project is distributed under the GNU General Public
+#           License version 3 (https://www.gnu.org/licenses/gpl-3.0.html)
+# Dependencies : Python 2.7
+##########################################################################
+
+import os
+
+CWD = os.getcwd()
+CSV_PATH = os.path.join(CWD,"csv")
